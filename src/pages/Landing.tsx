63eb88bc51bd66f1347e7f9ee33bb29cb158a870
yaneyba <yaneyba@gmail.com>
2025-07-08 09:09:58 -0500

    fix: remove SVG icon from demo badge in Landing component

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -32,9 +32,6 @@ const Landing: React.FC = () => {
       {isDemoMode && (
         <div className="fixed bottom-4 left-4 z-50 pointer-events-none">
           <div className="bg-blue-600 dark:bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg font-bold text-sm flex items-center pointer-events-auto">
-            <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 712 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-            </svg>
             DEMO
           </div>
         </div>
